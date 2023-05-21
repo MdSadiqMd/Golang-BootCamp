9ef4c73412cbe6f2a99d23a827d75eb86e779027
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-05-21 20:56:32 +0530

    Documentation

--- src/Booking Application/main.go
+++ src/Booking Application/main.go
@@ -5,10 +5,10 @@ func main(){
 	const ConferenceTickets=50 //If we Declare Const we cannot Change it in later it is final
 	var RemainingTickets=50
 
-	fmt.Println("Welcome to ",ConferenceName, " Booking Application")
-	fmt.Println("Get your Tickets")
-	fmt.Println("Total Tickets",ConferenceTickets,"Available Tickets",RemainingTickets)
-	fmt.Printf("Total Tickets %v Available Tickets %v",ConferenceTickets,RemainingTickets) //we use printf when format Specifier is present
+	fmt.Println("Welcome to ",ConferenceName, " Booking Application");
+	fmt.Println("Get your Tickets");
+	fmt.Println("Total Tickets",ConferenceTickets,"Available Tickets",RemainingTickets);
+	fmt.Printf("Total Tickets %v Available Tickets %v",ConferenceTickets,RemainingTickets);//we use printf when format Specifier is present
 
 
 }
--- src/main.go
+++ src/main.go
@@ -1,7 +1,12 @@
+/*
+Properties of Golang:
+Case sensitive
+Should Declare Varibles before use
+*/
 package main
 
 import "fmt"  //fmt is a package which contains the method for code ex:Println
 
 func main() {  //This is the main class from which the execution starts can be only one in a Code File
-    fmt.Println("Hello, World!") //Capital P
+    fmt.Println("Hello, World!"); //Capital P
 }
